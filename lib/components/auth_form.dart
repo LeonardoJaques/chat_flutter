1fd951fabb7cbd8d126c28b946b0f5b355e6964f
Leonardo Jaques <jaques.projetos@outlook.com>
2023-06-05 18:14:11 -0300

    feat: add auth_form

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,23 @@
+import 'package:chat_flutter/model/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    final isValid = _formkey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    _formkey.currentState?.save();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +26,53 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formkey,
           child: Column(
             children: [
+              if (_formData.isSignup)
+                TextFormField(
+                  key: const ValueKey('name'),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                  validator: (_name) {
+                    const minCharc = 5;
+                    final name = _name ?? '';
+                    if (name.trim().length < 5) {
+                      return 'Nome deve ter no mínimo $minCharc caracteres';
+                    }
+                    return null;
+                  },
+                ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Nome'),
-              ),
-              TextFormField(
+                key: const ValueKey('email'),
+                onChanged: (value) => _formData.email = value,
                 decoration: const InputDecoration(labelText: 'E-mail'),
+                validator: (value) {
+                  if (value == null || !value.contains('@')) {
+                    return 'Forneça um e-mail válido';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
+                key: const ValueKey('password'),
+                onChanged: (value) => _formData.password = value,
                 obscureText: true,
                 decoration: const InputDecoration(labelText: 'Senha'),
+                validator: (value) {
+                  const minCharc = 6;
+                  if (value == null || value.length < minCharc) {
+                    return 'Senha deve ter no mínimo $minCharc caracteres';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 12),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                    borderRadius: BorderRadius.circular(5),
                   ),
                   padding: const EdgeInsets.symmetric(
                     horizontal: 30,
@@ -35,11 +81,20 @@ class AuthForm extends StatelessWidget {
                   foregroundColor: Theme.of(context).colorScheme.onPrimary,
                   backgroundColor: Theme.of(context).colorScheme.primary,
                 ),
-                onPressed: () {},
-                child: const Text('Entrar'),
+                onPressed: _submit,
+                child: Text(_formData.isLogin ? 'Entrar' : 'Cadastrar'),
               ),
+              const SizedBox(height: 10),
               TextButton(
-                  onPressed: () {}, child: const Text('Criar uma nova conta'))
+                onPressed: () {
+                  setState(() {
+                    _formData.toggleAuthMode();
+                  });
+                },
+                child: Text(_formData.isLogin
+                    ? 'Criar uma nova conta'
+                    : 'Já tenho uma conta'),
+              )
             ],
           ),
         ),
