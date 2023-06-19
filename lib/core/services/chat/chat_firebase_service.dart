70c804fe688944033efb594b912ea5a3d010e973
Leonardo Jaques <jaques.projetos@outlook.com>
2023-06-19 14:41:20 -0300

    feat: add save msg of cha_firebase_service

---
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:chat_flutter/core/model/chat_message.dart';
+import 'package:chat_flutter/core/model/chat_user.dart';
+import 'package:chat_flutter/core/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFirebaseService implements ChatService {
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return const Stream<List<ChatMessage>>.empty();
+  }
+
+  @override
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+    // ChatMessage Map<String, dynamic>
+    final docRef = await store.collection('chat').add({
+      'text': text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': user.id,
+      'userName': user.name,
+      'userImageUrl': user.imageUrl,
+    });
+    final doc = await docRef.get();
+    // Map<String, dynamic> -> ChatMessage
+    final data = doc.data()!;
+    return ChatMessage(
+      id: doc.id,
+      text: data['text'],
+      createdAt: DateTime.parse(data['createdAt']),
+      userId: data['userId'],
+      userName: data['userName'],
+      userImageUrl: data['userImageUrl'],
+    );
+  }
+}
--- lib/core/services/chat/chat_service.dart
+++ lib/core/services/chat/chat_service.dart
@@ -1,11 +1,10 @@
 import 'package:chat_flutter/core/model/chat_message.dart';
 import 'package:chat_flutter/core/model/chat_user.dart';
-
-import 'chat_mock_service.dart';
+import 'package:chat_flutter/core/services/chat/chat_firebase_service.dart';
 
 abstract class ChatService {
   Stream<List<ChatMessage>> messagesStream();
-  Future<ChatMessage> save(String text, ChatUser user);
+  Future<ChatMessage?> save(String text, ChatUser user);
 
-  factory ChatService() => ChatMockService();
+  factory ChatService() => ChatFirebaseService();
 }
